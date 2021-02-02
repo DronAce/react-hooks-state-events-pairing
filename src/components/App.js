db1cf5b95974fd0e2fb99c494084c717d37e0420
DronAce <okeynazone@gmail.com>
2021-02-02 17:02:44 -0500

    done'

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
 import video from "../data/video.js";
-
 function App() {
-  console.log("Here's your data:", video);
-
+  const handleClick = (event) => {
+    const commentsDiv = document.querySelector(".comments");
+    commentsDiv.style.display === "none"
+      ? (commentsDiv.style.display = "block") &&
+        (event.target.textContent = "Hide Comments")
+      : (commentsDiv.style.display = "none") &&
+        (event.target.textContent = "Show Comments");
+  };
+  const commentUsers = video.comments.map((comment) => {
+    return (
+      <div key={comment.id}>
+        <p>{comment.user}</p>
+        <p>{comment.comment}</p>
+      </div>
+    );
+  });
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={video.embedUrl}
         frameborder="0"
         allowfullscreen
         title="Thinking in React"
       />
+      <h3>{video.title}</h3>
+      <p>
+        {video.views} Views | Uploaded {video.createdAt}
+      </p>
+      <button>{video.upvotes}</button>
+      <button>{video.downvotes}</button>
+      <div>
+        <br></br>
+        <button class="showHide" onClick={handleClick}>
+          Hide Comments
+        </button>
+      </div>
+      <div class="comments">
+        <h4>{video.comments.length} Comments</h4>
+        {commentUsers}
+      </div>
     </div>
   );
 }
-
 export default App;
